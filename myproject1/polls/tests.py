f172f3402c0760c0e1b28dcb9620b1b3d0440cf0
e9mgund <mayur.gund@enine.school>
2024-05-03 19:03:52 +0530

    Addded files till testing

--- myproject1/polls/tests.py
+++ myproject1/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import datetime
+from django.test import TestCase
+from django.utils import timezone
 
+from .models import Question
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+    def test_was_pubished_recently(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        new_question = Question(pub_date = time)
+        return self.assertIs(new_question.was_published_recently(),False)
